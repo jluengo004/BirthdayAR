3e4781dc4e09aa4ba1d95b23cb245c8361a20c9b
Jon Luengo <jon.luengo@csdlabs.com>
2019-12-17 00:10:56 +0100

    Almost every image set

---
+++ BirthdayAR/Project 0.09.51/SpotTheScientist/Pistas.swift
@@ -0,0 +1,15 @@
+//
+//  Scientist.swift
+//  SpotTheScientist
+//
+//  Created by Paul Hudson on 28/04/2019.
+//  Copyright © 2019 Hacking with Swift. All rights reserved.
+//
+
+import Foundation
+
+struct Pistas: Decodable {
+    let name: String
+    let bio: String
+    let country: String
+}
